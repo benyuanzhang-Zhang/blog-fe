64ba3d0b3f9b54f8bc11a4f665c1beeaec821d91
benyuanzhang <benyuanzhang@qq.com>
2020-06-21 01:41:22 +0800

    添加vue.config.ts

---
+++ vue.config.ts
@@ -0,0 +1,12 @@
+export default {
+  publicPath: './dist',
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3000',
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  }
+}

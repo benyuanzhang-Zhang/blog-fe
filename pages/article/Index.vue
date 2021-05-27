66ba0c64a9064691ada0aee73121c4c764adb529
benyuanzhang <benyuanzhang@qq.com>
2021-05-27 17:57:58 +0800

    init

---
+++ pages/article/Index.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>123</h1>
+</template>
+
+<script>
+export default {
+  name: "Index.vue"
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    123
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  async asyncData({ $axios, error }) {
+    try {
+      const { data } = await $axios.post('http://dz.hngqyun.cn/api/rtdb/st_rsvr_r/dp/rsvr_list')
+      return {
+        data
+      }
+    } catch (e) {
+      // error({
+      //   statusCode: 503,
+      //   message: 'server error'
+      // })
+    }
+  }
+})
+</script>
+
+<style>
+.container {
+  width: 100%;
+  height: 100%;
+}
+</style>
